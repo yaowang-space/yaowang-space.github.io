e2a2be3f36454ae69d7128af67bf8d52718d1938
Yao Wang <yaowang_cugb@163.com>
2026-06-07 23:17:10 +0800

    Fix dual theme toggles: mobile + desktop, update JS selectors
    - Mobile: toggle in nav-right div (always visible)
    - Desktop: toggle at end of visible-links (always visible)
    - JS: use class .theme-icon instead of #theme-icon (supports both)
    - JS: bind click to both #theme-toggle and #theme-toggle-desktop
    - Update minified JS accordingly

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -32,10 +32,10 @@ let setTheme = (theme) => {
 
   if (use_theme === "dark") {
     $("html").attr("data-theme", "dark");
-    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
+    $(".theme-icon").removeClass("fa-sun").addClass("fa-moon");
   } else if (use_theme === "light") {
     $("html").removeAttr("data-theme");
-    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
+    $(".theme-icon").removeClass("fa-moon").addClass("fa-sun");
   }
 };
 
@@ -100,7 +100,7 @@ $(document).ready(function () {
         });
 
   // Enable the theme toggle
-  $('#theme-toggle').on('click', toggleTheme);
+  $('#theme-toggle, #theme-toggle-desktop').on('click', toggleTheme);
 
   // Enable the sticky footer
   var bumpIt = function () {
